76c6db1e452e32bc03b3932f05518841dbb131eb
Ngozi Echegini <silasngoziechegini@gmail.com>
2025-09-20 19:21:56 -0400

    added proper asset mappings

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,21 +1,48 @@
-export { default as search_icon } from "./icons/search.svg";
-export { default as close_icon } from "./icons/close.svg";
-export { default as chevron_down_icon } from "./icons/chevron-down.svg";
-export { default as chevron_up_icon } from "./icons/chevron-up.svg";
-export { default as chevron_left_icon } from "./icons/chevron-left.svg";
-export { default as chevron_right_icon } from "./icons/chevron-right.svg";
-export { default as dropdown_arrow_icon } from "./icons/dropdown-arrow.svg";
-export { default as clear_icon } from "./icons/clear.svg";
-export { default as plus_icon } from "./icons/plus.svg";
-export { default as minus_icon } from "./icons/minus.svg";
-export { default as checkmark_icon } from "./icons/checkmark.svg";
-export { default as spinner_icon } from "./icons/spinner.svg";
-export { default as calendar_icon } from "./icons/calendar.svg";
-export { default as clock_icon } from "./icons/clock.svg";
-export { default as eye_icon } from "./icons/eye.svg";
-export { default as eye_off_icon } from "./icons/eye_off.svg";
-export { default as upload_icon } from "./icons/upload.svg";
-export { default as download_icon } from "./icons/download.svg";
-export { default as settings_icon } from "./icons/settings.svg";
-export { default as trash_icon } from "./icons/trash.svg";
-export { default as user_icon } from "./icons/user.svg";
+import { ReactComponent as searchIcon } from "./icons/search.svg";
+import { ReactComponent as closeIcon } from "./icons/close.svg";
+import { ReactComponent as chevronDownIcon } from "./icons/chevron-down.svg";
+import { ReactComponent as chevronUpIcon } from "./icons/chevron-up.svg";
+import { ReactComponent as chevronLeftIcon } from "./icons/chevron-left.svg";
+import { ReactComponent as chevronRightIcon } from "./icons/chevron-right.svg";
+import { ReactComponent as dropdownArrowIcon } from "./icons/dropdown-arrow.svg";
+import { ReactComponent as clearIcon } from "./icons/clear.svg";
+import { ReactComponent as plusIcon } from "./icons/plus.svg";
+import { ReactComponent as minusIcon } from "./icons/minus.svg";
+import { ReactComponent as checkmarkIcon } from "./icons/checkmark.svg";
+import { ReactComponent as spinnerIcon } from "./icons/spinner.svg";
+import { ReactComponent as calendarIcon } from "./icons/calendar.svg";
+import { ReactComponent as clockIcon } from "./icons/clock.svg";
+import { ReactComponent as eyeIcon } from "./icons/eye.svg";
+import { ReactComponent as eyeOffIcon } from "./icons/eye_off.svg";
+import { ReactComponent as uploadIcon } from "./icons/upload.svg";
+import { ReactComponent as downloadIcon } from "./icons/download.svg";
+import { ReactComponent as settingsIcon } from "./icons/settings.svg";
+import { ReactComponent as trashIcon } from "./icons/trash.svg";
+import { ReactComponent as userIcon } from "./icons/user.svg";
+import { FC } from "react";
+
+const iconSvgMapping: { [key: string]: FC<React.SVGProps<SVGSVGElement>> } = {
+  search_icon: searchIcon,
+  close_icon: closeIcon,
+  chevron_down_icon: chevronDownIcon,
+  chevron_up_icon: chevronUpIcon,
+  chevron_left_icon: chevronLeftIcon,
+  chevron_right_icon: chevronRightIcon,
+  dropdown_arrow_icon: dropdownArrowIcon,
+  clear_icon: clearIcon,
+  plus_icon: plusIcon,
+  minus_icon: minusIcon,
+  checkmark_icon: checkmarkIcon,
+  spinner_icon: spinnerIcon,
+  calendar_icon: calendarIcon,
+  clock_icon: clockIcon,
+  eye_icon: eyeIcon,
+  eye_off_icon: eyeOffIcon,
+  upload_icon: uploadIcon,
+  download_icon: downloadIcon,
+  settings_icon: settingsIcon,
+  trash_icon: trashIcon,
+  user_icon: userIcon,
+};
+
+export default iconSvgMapping;
