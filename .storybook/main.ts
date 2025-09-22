a5a322664c4cbe07da7235b809be2432bc8a970f
Ngozi Echegini <silasngoziechegini@gmail.com>
2025-09-21 21:49:21 -0400

    configurations for icons as svgs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import path from "path";
+import svgr from "vite-plugin-svgr";
 
 const config: StorybookConfig = {
   stories: [
@@ -15,6 +17,31 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+
+  viteFinal: async (config, { configType }) => {
+    config.plugins = [...(config.plugins || [])];
+
+    config.plugins.unshift(
+      svgr({
+        svgrOptions: {
+          exportType: "named", // to use ReactComponent export
+        },
+        include: "**/*.svg", // make sure this includes your SVGs
+      }),
+    );
+
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve?.alias,
+        "@assets": path.resolve(__dirname, "../src/assets"),
+        "@components": path.resolve(__dirname, "../src/components"),
+        "@utils": path.resolve(__dirname, "../src/utils"),
+        "@": path.resolve(__dirname, "../src"),
+      },
+    };
+    return config;
   },
 };
 export default config;
