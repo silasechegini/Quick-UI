1107119199c4513cb4698b7638f5a3d32e52a760
Ngozi Echegini <silasngoziechegini@gmail.com>
2025-10-01 22:01:10 -0400

    fixed failing unit tests

--- src/components/Flyout/__tests__/FlyoutHeader.spec.tsx
+++ src/components/Flyout/__tests__/FlyoutHeader.spec.tsx
@@ -1,6 +1,28 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { FlyoutHeader } from "../index";
+import FlyoutHeader from "../components/FlyoutHeader";
+import { vi } from "vitest";
+import { ButtonProps } from "@components/Button";
+
+// Mock the dependencies
+vi.mock("@components/Button", () => ({
+  Button: ({ children, onClick, icon, ...props }: ButtonProps) => (
+    <button onClick={onClick} {...props}>
+      {icon}
+      {children}
+    </button>
+  ),
+  BUTTON_SIZES: { EXTRASMALL: "xs" },
+  BUTTON_VARIANTS: { TERTIARY: "tertiary" },
+}));
+
+vi.mock("@assets", () => ({
+  iconSvgMapping: {
+    close_icon: ({ className }: { [key: string]: string }) => (
+      <svg className={className} data-testid="close-icon" />
+    ),
+  },
+}));
 
 describe("FlyoutHeader", () => {
   it("renders children correctly", () => {
@@ -9,7 +31,7 @@ describe("FlyoutHeader", () => {
   });
 
   it("renders a close button if onClose is provided", () => {
-    const handleClose = jest.fn();
+    const handleClose = vi.fn();
     render(<FlyoutHeader onClose={handleClose}>Title</FlyoutHeader>);
 
     const button = screen.getByRole("button");
