14e069c63b1bc73037ead1f90cd58678eb5b77eb
Ngozi Silas <silasngoziechegini@gmail.com>
2025-10-06 19:39:56 -0400

    Update comment for icon names import

--- stories/Icon.stories.tsx
+++ stories/Icon.stories.tsx
@@ -4,7 +4,7 @@ import { Meta, StoryObj } from "@storybook/react";
 import Icon from "../src/components/Icon/Icon";
 import { IconProps } from "../src/components/Icon/Icon.types";
 
-// 👇 Pull icon names dynamically (can hardcode for now if needed)
+// Pull icon names dynamically (can hardcode for now if needed)
 import { iconSvgMapping as Icons } from "../src/assets";
 
 const iconNames = Object.keys(Icons) as IconProps["name"][];
